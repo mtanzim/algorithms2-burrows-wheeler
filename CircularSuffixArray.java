e801abe944bb9ec2e401d33b2cab0fea166fd410
Tanzim Mokammel <tanzim.mokammel@diginex.com>
2019-10-22 17:06:09 +0200

    complete circular suffix array

--- CircularSuffixArray.java
+++ CircularSuffixArray.java
@@ -4,35 +4,67 @@
  *  Description: CircularSuffixArray for BW transform
  **************************************************************************** */
 
+import java.util.Arrays;
+
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
 public class CircularSuffixArray {
+    private int len;
+    private CircularSuffix suffixes[];
+
+    class CircularSuffix implements Comparable<CircularSuffix> {
+        int id;
+        String value;
+
+        public CircularSuffix(int id, String value) {
+            this.id = id;
+            this.value = value;
+
+        }
+
+        public int compareTo(CircularSuffix that) {
+            return value.compareTo(that.value);
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+    }
+
     // circular suffix array of s
     public CircularSuffixArray(String s) {
-        boolean debug = true;
-        if (debug)
-            StdOut.println(s);
-            StdOut.println(s.length());
-        for (int i = 0; i < s.length(); i++) {
-            if (debug) {
-                String sliceA = s.substring(i, s.length());
-                String sliceB = s.substring(0, i);
-                StdOut.println(sliceA+sliceB);
+        boolean debug = false;
+        len = s.length();
+        suffixes = new CircularSuffix[len];
+
+        for (int i = 0; i < len; i++) {
+            String sliceA = s.substring(i, s.length());
+            String sliceB = s.substring(0, i);
+            suffixes[i] = new CircularSuffix(i, sliceA + sliceB);
+        }
+        Arrays.sort(suffixes);
+        if (debug) {
+            for (int i = 0; i < len; i++) {
+                StdOut.println(suffixes[i].getId() + ":" + suffixes[i].value);
             }
         }
 
-        
     }
 
     // length of s
     public int length() {
-        return 0;
+        return len;
     }
 
     // returns index of ith sorted suffix
     public int index(int i) {
-        return 0;
+        return suffixes[i].getId();
     }
 
     // unit testing (required)
@@ -40,6 +72,9 @@ public class CircularSuffixArray {
         In in = new In(args[0]);
         String inputS = in.readAll();
         CircularSuffixArray sa = new CircularSuffixArray(inputS);
-        sa.length();
+        int len = sa.length();
+        for (int i = 0; i < len; i++) {
+            StdOut.println(sa.index(i));
+        }
     }
 }
