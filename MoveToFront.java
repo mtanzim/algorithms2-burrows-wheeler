5391fa10e23bf866844a905bf41bcbd41aef5f30
Tanzim Mokammel <tanzim.mokammel@diginex.com>
2019-10-29 18:11:58 +0100

    clean MoveToFront

--- MoveToFront.java
+++ MoveToFront.java
@@ -1,20 +1,14 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import edu.princeton.cs.algs4.BinaryStdIn;
 import edu.princeton.cs.algs4.BinaryStdOut;
-import edu.princeton.cs.algs4.StdOut;
 
 public class MoveToFront {
 
     // apply move-to-front encoding, reading from standard input and writing to
     // standard output
     public static void encode() {
-        // during testing
-        // boolean debug = false;
-        // String testString = "CAAABCCCACCF";
         char moveToFront[] = new char[256];
-
         // initialize
         for (int i = 0; i < 256; i++) {
             moveToFront[i] = ((char) i);
@@ -24,8 +18,7 @@ public class MoveToFront {
             char curChar = BinaryStdIn.readChar();
             for (int j = 0; j < moveToFront.length; j++) {
                 if (curChar == moveToFront[j]) {
-                    // out[i] = j;
-                    outL.add((char)j);
+                    outL.add((char) j);
                     // move jth character to the front
                     // shift everything else
                     for (int k = j; k > 0; k--) {
@@ -37,48 +30,24 @@ public class MoveToFront {
                 }
             }
         }
-        // if (debug) {
-        //     StdOut.println(Arrays.toString(in));
-        //     StdOut.println(Arrays.toString(outL.toArray()));
-        //     for (int i = 0; i < outL.size(); i++) {
-        //         StdOut.print(moveToFront[i]);
-        //     }
-        //     StdOut.println();
-        // }
         for (char outC : outL) {
-            // StdOut.print(outC);
             BinaryStdOut.write(outC);
         }
         BinaryStdOut.flush();
-
     }
 
     // apply move-to-front decoding, reading from standard input and writing to
     // standard output
     public static void decode() {
-        // boolean debug = true;
-        // boolean testing = false;
-        // int testSeq[] = new int[] { 77, 89, 34, 79, 68, 4, 72, 4, 76, 84, 2, 78, 111, 111, 99, 115, 45, 0, 0, 0 };
-
-        // int in[] = new int[500];
-        // char out[] = new char[500];
-
         // array of ascii chars
         char moveToFront[] = new char[256];
         // initialize
         for (int i = 0; i < 256; i++) {
             moveToFront[i] = ((char) i);
         }
-
-        // if (testing) {
-        //     in = testSeq;
-        //     out = new char[in.length];
-        // }
-
         // decode
         ArrayList<Character> outL = new ArrayList<Character>();
         while (!BinaryStdIn.isEmpty()) {
-        // for (int i = 0; i < in.length; i++) {
             int curIdx = (int) BinaryStdIn.readChar();
             char curChar = moveToFront[curIdx];
             outL.add(curChar);
@@ -91,35 +60,14 @@ public class MoveToFront {
 
         }
         for (char outC : outL) {
-            // StdOut.print(outC);
             BinaryStdOut.write(outC);
         }
         BinaryStdOut.flush();
-        // if (debug) {
-        //     StdOut.println(Arrays.toString(in));
-        //     StdOut.println(Arrays.toString(out));
-        //     for (int i = 0; i < in.length; i++) {
-        //         StdOut.print(moveToFront[i]);
-        //     }
-        //     StdOut.println();
-        // }
     }
 
     // if args[0] is "-", apply move-to-front encoding
     // if args[0] is "+", apply move-to-front decoding
     public static void main(String[] args) {
-
-        boolean debug = false;
-        boolean testing = false;
-
-        if (testing) {
-            encode();
-            decode();
-            return;
-        }
-        if (debug)
-            StdOut.println(args[0]);
-
         if (args[0].compareTo("-") == 0)
             encode();
         else if (args[0].compareTo("+") == 0)
